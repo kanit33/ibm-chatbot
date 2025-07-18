2bcad3798b6f7803e1dfec67ab03c30a262ff5e6
kanit33 <kanitgohel33@gmail.com>
2025-07-18 16:21:51 +0530

    Fixed

--- app.py
+++ app.py
@@ -44,5 +44,7 @@ def chat():
     except Exception as e:
         return jsonify({"reply": "Error connecting to Groq API."}), 500
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
